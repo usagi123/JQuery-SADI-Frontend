17929b797e703fd30b2355deb1750a7183bbcc33
usagi123 <maipham.quanghuy@gmail.com>
2018-01-04 02:09:19 +0700

    No Oauth + Updated JS

--- main.js
+++ main.js
@@ -1,250 +1,143 @@
 var access_token = ''
 
 //This is an ajax request. The purpsose is to get an access_token
-// $.ajax({
-//     url: 'http://localhost:8080/oauth/token?grant_type=password&username=admin&password=admin',
-//     method: 'post',
-//     beforeSend: function(xhr){
-//         xhr.setRequestHeader('Authorization','Basic Y2xpZW50LWlkOnNlY3JldA==')
-// },
-//     success: function(data){
-//         setCookie("access_token", data.access_token, 1)
-//         console.log(getCookie("access_token"))
-//         $('body').append("<div>Login successfully. <a href='index.html'>Go to main page</a></div>")
-// }
-// })
+$.ajax({
+    url: 'http://localhost:8080/oauth/token?grant_type=password&username=admin&password=admin',
+    method: 'post',
+    beforeSend: function(xhr){
+        xhr.setRequestHeader('Authorization','Basic Y2xpZW50LWlkOnNlY3JldA==')
+},
+    success: function(data){
+        setCookie("access_token", data.access_token, 1)
+        console.log(getCookie("access_token"))
+        $('body').append("<div>Login successfully. <a href='index.html'>Go to main page</a></div>")
+}
+})
 
-// function setCookie(cname, cvalue, exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-//     var expires = "expires="+ d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for(var i = 0; i <ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "Empty";
-// }
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "Empty";
+}
 
-//Question CRUD
-function QuestionService(){
+//QUESTION CRUD
+$(function(){
     var $questions = $('#questions');
     var $qid = $('#qid');
-    var $question = $('#question');
+    var $content = $('#content');
 
-    var questionTemplate = $('question-template').html();
+    var questionTemplate = $('#question-template').html();
 
-    function addNewQuestion(question){
-        $questions.append(Mustache.render(questionTemplate), question);            
+    function addQuestion(question){
+        $questions.append(Mustache.render(questionTemplate, question));
     }
 
-    //Read
     $.ajax({
         type: 'GET',
         url: 'localhost:8080/questions/getAll',
-        success: function(question){
-            console.log('success', question)
-            $.each(question, function(qid, question){
-                addNewQuestion(question);
+        success: function(questions) {
+            console.log("Getted all questions", questions); //debug
+            $.each(questions, function(i, question){
+                addQuestion(question);
             });
         },
         error: function(){
-            alert('Error loading questions');
+            alert('Error loading questions')
         }
     });
 
-    //Create
-    $('add_question').on('click', function(){
-        var addQuestion = {
+    $('#add-question').on('click', function(){
+
+        var question = {
             qid: $qid.val(),
-            question: $question.val(),
-        };
+            content: $content.val(),
+        }
+
         $.ajax({
             type: 'POST',
             url: 'localhost:8080/questions/create',
-            data: addQuestion,
-            success: function(newQuestion){
-                addNewQuestion(newQuestion);
+            data: question,
+            success: function(newQuestion) {
+                console.log("Question added!", newQuestion); //debug
+                addQuestion(newQuestion);
             },
             error: function(){
-                alert('Error saving question');
-            }
-        })
-    });
-
-    //Delete
-    $questions.delegate('.remove','click', function(){
-
-        var $li = $(this).closest('li');
-        $.ajax({
-            type: 'DELETE',
-            url: 'localhost:8080/questions/deleteByqid',
-            success: function(){
-                $li.fadeOut(300, function(){
-                    $(this).remove();
-                });
+                alert('Error adding question')
             }
         });
     });
 
-    //Update
-    $questions.delegate('.editQuestion', 'click', function(){
+    $questions.delegate('.remove', 'click', function(){
+
         var $li = $(this).closest('li');
-        $li.find('input.qid').val($li.find('span.qid').html());
-        $li.find('input.question').val($li.find('span.question').html());
-        $li.addClass('edit');
-    });
 
-    $questions.delegate('.cancelEdit', 'click', function(){
-        $(this).closest('li').removeClass('edit');
-    })
-
-    $questions.delegate('saveEdit', 'click', function(){
-        var $li = $(this).closest('li');
-        var question = {
-            qid: $li.find('input.qid').val(),
-            question: $li.find('input.question').val()
-        };
-
-        var updateQuestion = {
-            qid: $qid.val(),
-            question: $question.val(),
-        };
         $.ajax({
-            type: 'PUT',
-            url: 'localhost:8080/questions/updateByqid',
-            data: updateQuestion,
-            success: function(newQuestion){
-                $li.find('span.qid').html(question.qid);
-                $li.find('span.question').html(question.question);
-                $li.removeClass('edit');
-            },
-            error: function(){
-                alert('Error updating question');
+            type: 'DELETE',
+            // url: 'localhost:8080/questions/deleteByqid' + $(this).attr('data-id'),
+            // url: 'localhost:8080/questions/deleteByqid' + $(this).val(question.qid),
+            // url: 'localhost:8080/questions/deleteByqid' + $(this).val('qid'),
+            // url: 'localhost:8080/questions/deleteByqid' + $li.find('input.qid').val($li.find('span.qid').html()),
+            // url: 'localhost:8080/questions/deleteByqid' + $(this).val($li.find('span.qid').html()),
+            url: 'localhost:8080/questions/deleteByqid' + $li.attr('data-id'),
+            success: function(){
+                console.log('Question Deleted Successfully!');
+                $li.fadeOut(300, function(){
+                    $(this).remove();
+                })
             }
         })
     })
-};
 
-// //Answer CRUD
-// function AnswerService(){
-//     var $answers = $('#answers');
-//     var $aid = $('#aid');
-//     var $answer = $('#answer');
-//     var $correctcheck = $('#correctcheck');
-//     var $qid = $('qid');
+    $questions.delegate('.editQuestion', 'click', function(){
+         var $li = $(this).closest('li');
+         $li.find('input.qid').val($li.find('span.qid').html());
+         $li.find('input.content').val($li.find('span.content').html());
+         $li.addClass('edit');
 
-//     var answerTemplate = $('answer-template').html();
+    });
 
-//     function addNewAnswer(answer){
-//         $answers.append(Mustache.render(answerTemplate), answer);            
-//     }
+    $questions.delegate('.cancelEdit', 'click', function(){
+        $(this).closest('li').removeClass('edit')
 
-//     //Read
-//     $.ajax({
-//         type: 'GET',
-//         url: 'localhost:8080/answers/getAll?access_token='+getCookie('access_token'),
-//         success: function(answer){
-//             console.log('success', answer)
-//             $.each(answer, function(aid, answer){
-//                 addNewAnswer(answer);
-//             });
-//         },
-//         error: function(){
-//             alert('Error loading answers');
-//         }
-//     });
+    });
 
-//     //Create
-//     $('add_answer').on('click', function(){
-//         var addAnswer = {
-//             aid: $answer.val(),
-//             answer: $answer.val(),
-//             correctcheck: $answer.val(),
-//             qid: $qid.val(),
-//         };
-//         $.ajax({
-//             type: 'POST',
-//             url: 'localhost:8080/answers/create?access_token='+getCookie('access_token'),
-//             data: addAnswer,
-//             success: function(newAnswer){
-//                 addNewQuestion(newAnswer);
-//             },
-//             error: function(){
-//                 alert('Error saving answer');
-//             }
-//         })
-//     });
+    $questions.delegate('.saveEdit', 'click', function(){
+        var $li = $(this).closest('li');
+        var question = {
+            qid: $li.find('input.qid').val(),
+            age: $li.find('input.content').val(),
+        }
 
-//     //Delete
-//     $answers.delegate('.remove','click', function(){
-
-//         var $li = $(this).closest('li');
-//         $.ajax({
-//             type: 'DELETE',
-//             url: 'localhost:8080/answers/deleteByaid' + $(this).attr('data-id') + '?access_token='+getCookie('access_token'),
-//             success: function(){
-//                 $li.fadeOut(300, function(){
-//                     $(this).remove();
-//                 });
-//             }
-//         });
-//     });
-
-//     //Update
-//     $answers.delegate('.editAnswer', 'click', function(){
-//         var $li = $(this).closest('li');
-//         $li.find('input.aid').val($li.find('span.aid').html());
-//         $li.find('input.answer').val($li.find('span.answer').html());
-//         $li.find('input.correctcheck').val($li.find('span.correctcheck').html());
-//         $li.find('input.qid').val($li.find('span.qid').html());
-//         $li.addClass('edit');
-//     });
-
-//     $answers.delegate('.cancelEdit', 'click', function(){
-//         $(this).closest('li').removeClass('edit');
-//     })
-
-//     $answers.delegate('saveEdit', 'click', function(){
-//         var $li = $(this).closest('li');
-//         var answer = {
-//             aid: $li.find('input.aid').val(),
-//             question: $li.find('input.question').val(),
-//             correctcheck: $li.find('input.correctcheck').val(),
-//             qid: $li.find('input.qid').val(),
-//         };
-
-//         var updateAnswer = {
-//             aid: $aid.val(),
-//             answer: $answer.val(),
-//             correctcheck: $answer.val(),
-//             qid: $qid.val(),
-//         };
-//         $.ajax({
-//             type: 'PUT',
-//             url: 'localhost:8080/questions/updateByaid?access_token='+getCookie('access_token'),
-//             data: updateAnswer,
-//             success: function(newAnswer){
-//                 $li.find('span.aid').html(answer.aid);
-//                 $li.find('span.answer').html(answer.answer);
-//                 $li.find('span.correctcheck').html(answer.correctcheck);
-//                 $li.find('span.qid').html(question.qid);
-//                 $li.removeClass('edit');
-//             },
-//             error: function(){
-//                 alert('Error updating answer');
-//             }
-//         })
-//     })
-// };
+        $.ajax({
+            type: 'PUT',
+            url: 'localhost:8080/questions/updateByqid' + $li.attr('data-id'),
+            data: question,
+            success: function(newQuestion) {
+                console.log("Question editted!", newQuestion); //debug
+                $li.find('span.qid').html(question.qid);
+                $li.find('span.content').html(question.content);
+                $li.removeClass('edit')
+            },
+            error: function(){
+                alert('Error updating question')
+            }
+        });
+    });
+})
